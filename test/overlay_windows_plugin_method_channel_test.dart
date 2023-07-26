e661abe515bfc54c3a07154e8b1bb45adecbeb1a
Dang Dinh Duc <ducdd.it@gmail.com>
2023-07-26 15:37:17 +0700

    remove redundant files

--- test/overlay_windows_plugin_method_channel_test.dart
+++ test/overlay_windows_plugin_method_channel_test.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:overlay_windows_plugin/overlay_windows_plugin_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:overlay_windows_plugin/overlay_windows_plugin_method_channel.dart';
 
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+// void main() {
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelOverlayWindowsPlugin platform = MethodChannelOverlayWindowsPlugin();
-  const MethodChannel channel = MethodChannel('overlay_windows_plugin');
+//   MethodChannelOverlayWindowsPlugin platform = MethodChannelOverlayWindowsPlugin();
+//   const MethodChannel channel = MethodChannel('overlay_windows_plugin');
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
+//   setUp(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+//       channel,
+//       (MethodCall methodCall) async {
+//         return '42';
+//       },
+//     );
+//   });
 
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
+//   tearDown(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+//   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
-}
+//   test('getPlatformVersion', () async {
+//     expect(await platform.getPlatformVersion(), '42');
+//   });
+// }
--- test/overlay_windows_plugin_test.dart
+++ test/overlay_windows_plugin_test.dart
@@ -1,25 +1,19 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:overlay_windows_plugin/overlay_windows_plugin_method_channel.dart';
-import 'package:overlay_windows_plugin/overlay_windows_plugin_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockOverlayWindowsPluginPlatform with MockPlatformInterfaceMixin implements OverlayWindowsPluginPlatform {
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+// class MockOverlayWindowsPluginPlatform with MockPlatformInterfaceMixin implements OverlayWindowsPluginPlatform {
+//   @override
+//   Future<String?> getPlatformVersion() => Future.value('42');
+// }
 
-void main() {
-  final OverlayWindowsPluginPlatform initialPlatform = OverlayWindowsPluginPlatform.instance;
+// void main() {
+//   final OverlayWindowsPluginPlatform initialPlatform = OverlayWindowsPluginPlatform.instance;
 
-  test('$MethodChannelOverlayWindowsPlugin is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelOverlayWindowsPlugin>());
-  });
+//   test('getPlatformVersion', () async {
+//     // OverlayWindowsPlugin overlayWindowsPlugin = OverlayWindowsPlugin();
+//     // MockOverlayWindowsPluginPlatform fakePlatform = MockOverlayWindowsPluginPlatform();
+//     // OverlayWindowsPluginPlatform.instance = fakePlatform;
 
-  test('getPlatformVersion', () async {
-    // OverlayWindowsPlugin overlayWindowsPlugin = OverlayWindowsPlugin();
-    // MockOverlayWindowsPluginPlatform fakePlatform = MockOverlayWindowsPluginPlatform();
-    // OverlayWindowsPluginPlatform.instance = fakePlatform;
-
-    // expect(await overlayWindowsPlugin.getPlatformVersion(), '42');
-  });
-}
+//     // expect(await overlayWindowsPlugin.getPlatformVersion(), '42');
+//   });
+// }
